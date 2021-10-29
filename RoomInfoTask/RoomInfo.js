a7f4725cc23b10d69fabe7e78a225cb2262cb251
Asfand-Dev <gulshierkhan@gmail.com>
2021-10-29 14:23:57 -0400

    "adding rooms soon"

--- RoomInfoTask/RoomInfo.js
+++ RoomInfoTask/RoomInfo.js
@@ -78,3 +78,43 @@ btn[1].addEventListener("click", function () {
         content.style.display = "block";
     }
 });
+
+btn[2].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
+
+btn[3].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
+
+btn[4].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
+
+btn[5].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
