a237c00d3754e449820b23b2ffa0e9b9339f0061
Asfand-Dev <gulshierkhan@gmail.com>
2021-10-29 20:46:40 -0400

    major modal changes

--- CourseDirectoryTask/CourseDirectory.js
+++ CourseDirectoryTask/CourseDirectory.js
@@ -1,8 +1,30 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+function showMap(id) {
+    if (id == 1207) {
+        document.getElementById("map").src = "../assets/Final_GroundFloor.png";
+    } else if (id == 0102) {
+        document.getElementById("map").src = "../assets/Final_GroundFloor.png";
+    }
 }
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+
+var modal = document.getElementById("mapModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function showMapModal(id) {
+    modal.style.display = "block";
+    showMap(id)
+}
+
+// When the user clicks on x, close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 var btn = document.getElementsByClassName("coursebtn");
--- RoomInfoTask/RoomInfo.js
+++ RoomInfoTask/RoomInfo.js
@@ -1,46 +1,16 @@
-
-const roomCoords = new Map();
-
-roomCoords.set('0102', [420, 512]);
-roomCoords.set('0108', [200, 420]);
-roomCoords.set('0198', [550, 320]);
-roomCoords.set('0324', [560, 150]);
-//roomCoords.set('1104', [x, y]);
-//roomCoords.set('1119', [x, y]);
-//roomCoords.set('1124', [x, y]);
-
-
 function showMap(id) {
-
-    floorString = id.substring(0, 1);
-    floorNum = parseInt(floorString);
-    console.log(floorNum);
-    switch (floorNum) {
-        case 0:
-            document.getElementById("map").src = "../assets/Final_GroundFloor.png";
-            var coords = roomCoords.get(id)
-            document.getElementById("map").onload = put_marker(coords[0], coords[1]);
-            break;
-        case 1:
-            document.getElementById("map").src = floorPic = "../assets/Final_FloorOne.png";
-            break;
+    if (id == 1207) {
+        document.getElementById("map").src = "../assets/Final_GroundFloor.png";
+    } else if (id == 0102) {
+        document.getElementById("map").src = "../assets/Final_GroundFloor.png";
     }
 }
 
-/*function put_marker(from_left, from_top) {
-    var marker = document.getElementById('marker');
-    marker.style.left = from_left + "px";
-    marker.style.top = from_top + "px";
-    marker.display = "block";
-};
-*/
 var modal = document.getElementById("mapModal");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 function showMapModal(id) {
-    console.log("Test");
-    console.log(id);
     modal.style.display = "block";
     showMap(id)
 }
