ce319c1ed4af0335b1e9e2c558b06a4fe77a844b
AustinBurch <austinaburch@gmail.com>
2021-11-09 20:56:03 -0500

    corrected modal for campus map

--- home.js
+++ home.js
@@ -1,35 +1,34 @@
-var modal = document.getElementById("HungryModal");
-var campusMapModal = document.getElementById("CampusMapModal");
-
-// Get the button that opens the modal
-
-
+var modal = document.getElementsByClassName("modal")[1];
+var campusMapModal = document.getElementsByClassName("modal")[0];
+console.log(document.getElementsByClassName("modal"));
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
+var span = document.getElementsByClassName("close")[1];
+var campus_span = document.getElementsByClassName("close")[0];
+console.log(document.getElementsByClassName("close"));
 // When the user clicks on the button, open the modal
 function showHungryModal()
-{
-    
-    
+{    
     modal.style.display = "block";
         // When the user clicks on x, close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
 
 function showCampusModal() {
     campusMapModal.style.display = "block";
     // When the user clicks on x, close the modal
-span.onclick = function () {
+}
+
+campus_span.onclick = function () {
     campusMapModal.style.display = "none";
 }
 
@@ -39,9 +38,6 @@ window.onclick = function (event) {
         campusMapModal.style.display = "none";
     }
 }
-}
-
-
 
 var imgNr = 0;
 var images = ["./assets/Final_GroundFloor.png", "./assets/Final_FloorOne.png", "./assets/Final_FloorTwo.png", 
